b8fe1d933311659e5075c65fbad9114836b19d86
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-07 13:10:54 -0400

    phase-1-destructuring-assignment/
    phase-1-destructuring-assignment/

--- phase-1-destructuring-assignment-master/index.js
+++ phase-1-destructuring-assignment-master/index.js
@@ -11,32 +11,40 @@ const { muppetName, color, song ,job,  partner} = muppet;
 
 
 
-
-
-
-const nestedMuppet = {  ``
+const nestedMuppet = {
   nestedName: 'Kermit',
   nestedColor: 'green',
-  album: {
+  album : {
     theMuppetMovie: {
-      song1: 'Rainbow Connection',
-      song2: 'Moving Right Along',
-      song3: 'Never Before, Never Again',
-      song4: 'I Hope That Something Better Comes Along',
-    },
+      song1:  'Rainbow Connection',
+      song2:  'Moving Right Along',
+      song3:  'Never Before,  Never Again',
+      song4:  'I Hope That Something Better Comes Along',
   },
-  nestedJob: 'Host of The Muppet Show',
-  nestedPartner: 'Miss Piggy'
+},
+nestedJob: 'Host of The Muppet SHow',
+nestedPartner: 'Miss Piggy'
+
+};
+
+
+const {muppetName, color, song ,job,  partner} = muppet;
+
+
+
+
+const muppet = {
+  muppetName: 'Miss Piggy',
+  color: 'pink',
+  song: 'Never Before, Never Again',
+  job: 'Cast member of The Muppet Show',
+  partner: 'Kermit'
 };
 
 
 
 
-
-
-const { muppetName, color, song ,job,  partner} = muppet;
-
-
+const { muppetName, color, song, job, partner } = muppet;
 
 
 
@@ -56,10 +64,6 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
-
-
-
-
 
 // Strings
 
