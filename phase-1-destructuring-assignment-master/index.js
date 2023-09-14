34bc66db3ab6c786818cd849280ba21abf1330e0
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-13 20:52:38 -0400

    phase-1-destructuring-assignment/
    phase-1-destructuring-assignment/

--- phase-1-destructuring-assignment-master/index.js
+++ phase-1-destructuring-assignment-master/index.js
@@ -1,57 +1,54 @@
+Certainly! I apologize for the confusion earlier. Let's fix the code snippets for you:
+
+// Code snippet 1 - Destructuring assignment with string
+
+const dogsName = 'Sir Woody BarjsAlot';
+const [title, , lastName] = dogsName.split(' ');
+console.log(title, lastName); // LOG: Sir BarjsAlot
+
+// Code snippet 2 - Destructuring assignment with object
+
 const doggie = {
   name: 'Buzz',
   breed: 'Great Pyrenees',
   furColor: 'black and white',
   activityLevel: 'sloth-like',
   favoriteFood: 'hot dogs'
-  };
-  
-  const { name, breed } = doggie;
-  name; // => "Buzz"
-  breed; // => "Great Pyrenees"
-
-  
-const doggie = {
-name: 'Buzz',
-breed: 'Great Pyrenees',
-furColor: 'black and white',
-activityLevel: 'sloth-like',
-favoriteFood: 'hot dogs'
 };
-
 const { name, breed } = doggie;
-name; // => "Buzz"
-breed; // => "Great Pyrenees"
-``
+console.log(name); // Output: Buzz
+console.log(breed); // Output: Great Pyrenees
 
+// Code snippet 3 - Destructuring assignment with nested object
 
 const doggie = {
-name: 'Buzz',
-breed: 'Great Pyrenees',
-furColor: 'black and white',
-activityLevel: 'sloth-like',
-favoriteFoods: {
-meats:{
-ham: 'smoked',
-hotDog: 'Oscar Meyer',
-},
-cheeses:{
-american: 'kraft'
-}
-}
+  name: 'Buzz',
+  breed: 'Great Pyrenees',
+  furColor: 'black and white',
+  activityLevel: 'sloth-like',
+  favoriteFoods: {
+    meats: {
+      ham: 'smoked',
+      hotDog: 'Oscar Meyer',
+    },
+    cheeses: {
+      american: 'kraft'
+    }
+  }
 };
 
 const { ham, hotDog } = doggie.favoriteFoods.meats;
-ham; // => "smoked"
-hotDog; // => "Oscar Meyer"
-``
-javascript
+console.log(ham); // Output: smoked
+console.log(hotDog); // Output: Oscar Meyer
+
+// Code snippet 4 - Destructuring assignment with array
+
 const dogs = ['Great Pyrenees', 'Pug', 'Bull Mastiff'];
 const [, small, giant] = dogs;
 console.log(small, giant); // LOG: Pug Bull Mastiff
 
+// Code snippet 5 - Destructuring assignment with array from string split
+
 const dogsName = 'Sir Woody BarksALot';
 const [title, firstName, lastName] = dogsName.split(' ');
-console.log(title, firstName, lastName); // LOG: Sir Woody BarksALot
-
-
+console.log(title, firstName, lastName); // LO
